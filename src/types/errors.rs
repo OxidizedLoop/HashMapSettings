946ce5238ed854884667b04fe4f357d4644f0dac
OxidizedLoop <oxidizedloop@gmail.com>
2023-11-01 19:52:29 +0000

    added derive PartialEq to Error enums for assert!

--- src/types/errors.rs
+++ src/types/errors.rs
@@ -1,13 +1,16 @@
+#[derive(PartialEq)]
 pub enum CacheError {
     Renaming,
     Inserting,
     Naming,
 }
+#[derive(PartialEq)]
 pub enum DeepChangeError {
     Cache(CacheError),
     NotFound,
     EmptyVec,
 }
+#[derive(PartialEq)]
 pub enum InvalidAccountError {
     Cache(CacheError),
     ExistingName,
