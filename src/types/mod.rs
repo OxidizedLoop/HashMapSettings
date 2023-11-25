bb95cf6b5a2322704feba7075c9add24f68029d7
OxidizedLoop <oxidizedloop@gmail.com>
2023-11-25 17:55:13 +0000

    `doc` added description to modules

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,3 +1,6 @@
+///module containing Constants
 pub mod constants;
+///module containing Errors
 pub mod errors;
+///module containing implementations of `Setting` for rust types
 pub mod rust_types_implementations;
