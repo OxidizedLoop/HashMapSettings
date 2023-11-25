1b9cdf622a8fdc9302670e978304fb2a304353bd
OxidizedLoop <oxidizedloop@gmail.com>
2023-11-25 18:00:14 +0000

    `doc`, renamed, and derived Debug for Errors in Error module
    renames in libs.rs will be committed in future commits

--- src/types/errors.rs
+++ src/types/errors.rs
@@ -1,19 +1,32 @@
-#[derive(PartialEq)]
+/// Errors involving interaction with `Cache`
+#[derive(Debug, PartialEq)]
 pub enum CacheError {
+    /// Error of trying to rename `Cache`
     Renaming,
-    Inserting,
-    Changing,
+    /// Error of trying to create a `Cache` without using [`cache()`](Account::cache)
+    Creating,
+    /// Error of trying to modify the contents of `Cache`
+    Modify,
+    /// Error of trying to name a Account `Cache`
     Naming,
 }
-#[derive(PartialEq)]
+/// Errors involving Deep Functions
+#[derive(Debug, PartialEq)]
 pub enum DeepChangeError {
+    /// Errors involving interaction with `Cache`
     Cache(CacheError),
+    /// Error of providing a name of a sub Account that doesn't exist
     NotFound,
+    /// Error of providing a empty `Vec` to a deep function
     EmptyVec,
 }
-#[derive(PartialEq)]
+/// Errors involving Deep Functions
+#[derive(Debug, PartialEq)]
 pub enum InvalidAccountError {
+    /// Errors involving interaction with `Cache`
     Cache(CacheError),
+    /// Error of trying to do a action that will lead to two `Accounts` having the same name.
     ExistingName,
+    /// Error of trying to create an account where `Cache` isn't the last element of the `Vec`
     WronglyPositionedCache,
 }
